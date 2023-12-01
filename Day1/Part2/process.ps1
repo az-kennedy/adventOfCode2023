9fcf6daab0494a4532c7eb67747b6c78dc760fb1
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2023-12-01 11:19:43 -0500

    updated

---
+++ Day1/Part2/process.ps1
@@ -0,0 +1,19 @@
+$data = Get-Content -Path '/Users/tkennedy/REPOS/adventOfCode2023/Day1/Part1/input.txt'
+
+$sum = 0
+
+foreach ($code in $data){
+  $num = ($code -replace "[^0-9]" , '')
+  
+  if($num.Length -eq 1){
+    $num = $num + $num
+  }
+
+  if ($num.Length -gt 2) {
+    $num = $num[0] + $num[-1]
+  }
+
+  $sum += [int]$num
+}
+
+Write-Output $sum
