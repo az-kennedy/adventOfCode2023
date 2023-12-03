f827a983d709dabc5844a2f41b992b293d010228
Thomas Kennedy <doomedsififreak@gmail.com>
2023-12-03 15:13:35 -0500

    populated hash

--- Day3/Part1/process.ps1
+++ Day3/Part1/process.ps1
@@ -1,3 +1,35 @@
-$data = Get-Content -Path 'C:\Users\Thomas\REPOs\adventOfCode2023\Day2\Part1\input.txt'
+$data = Get-Content -Path 'C:\Users\Thomas\Documents\code\REPOS\adventOfCode2023\Day3\Part1\input.txt'
+
+$sum = 0
+
+$rowHash = @{}
+
+foreach ($row in $data) {
+  # Populate Hash...
+  if ($rowHash.Count -eq 0) {
+    $rowHash[1] = $row
+  } elseif ($rowHash.Count -eq 1) {
+    $rowHash[2] = $row
+  } elseif ($rowHash.Count -eq 2) {
+    $rowHash[3] = $row
+  }elseif ($rowHash.Count -eq 3) {
+    #Add new row, take one out, shift rows
+    $rowHash[3] = $rowHash[2]
+    $rowHash[2] = $rowHash[1]
+    $rowHash[1] = $row
+  }
+  Write-Host "----------------------------"
+  foreach ($row in $rowHash.GetEnumerator()) {
+    Write-Host "$($row.key) : $($row.value)"
+  }
+  Write-Host "----------------------------"
 
 
+}
+
+
+
+#Print Hash
+# foreach ($row in $rowHash.GetEnumerator()) {
+#   Write-Host "$($row.key) : $($row.value)"
+# }
